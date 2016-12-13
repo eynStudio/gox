d3a0fd26e714957dc5458f40e5579d7fe69b5140
pswang <pengshengwang@gmail.com>
2016-12-13 21:16:21 +0800

    add

---
+++ x/intx/intx.go
@@ -0,0 +1,19 @@
+package intx
+
+func Contains(lst []int, v int) bool {
+	for _, it := range lst {
+		if it == v {
+			return true
+		}
+	}
+	return false
+}
+
+func Intersect(lst1, lst2 []int) (lst []int) {
+	for _, it := range lst {
+		if Contains(lst2, it) {
+			lst = append(lst, it)
+		}
+	}
+	return
+}
